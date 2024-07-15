daeb68185d213a0dfc29976c8e76206228adac5d
Samanyu24X <35706393+Samanyu24X@users.noreply.github.com>
2024-07-14 22:37:37 -0700

    updated website with new UI and projects

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
-// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-//     anchor.addEventListener('click', function (e) {
-//       e.preventDefault();
-  
-//       const target = document.querySelector(this.getAttribute('href'));
-//       if (target) {
-//         target.scrollIntoView({
-//           behavior: 'smooth'
-//         });
-//       }
-//     });
-//   });
-  
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-anchor.addEventListener('click', function (e) {
-    e.preventDefault();
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
-    const targetId = this.getAttribute('href').substring(1);
-    const target = document.getElementById(targetId);
+        const targetId = this.getAttribute('href').substring(1);
 
-    if (target) {
-    target.scrollIntoView({
-        behavior: 'smooth'
+        if (targetId === 'home') {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        } else {
+            const target = document.getElementById(targetId);
+            if (target) {
+                target.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        }
     });
-    }
-});
 });
